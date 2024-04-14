d8e1b0cfb74173ce8a97770360df913463dd021e
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-14 19:43:25 +0530

    add tailwind and shadcn in shared ui

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,10 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-    tsconfigRootDir: __dirname,
-  },
-};
+    root: true,
+    extends: ['@secret-hub/eslint-config/react-internal.js'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.lint.json',
+        tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: ['node_modules/', 'dist/', '**/*.d.ts', '*.config.js'],
+}
