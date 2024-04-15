edbfe3f3368a3ffcda5013898f606c64ac249f70
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-15 09:26:05 +0530

    Add migration for creating User and Message models

---
+++ packages/db/prisma/migrations/20240415035321_created_user_and_message_model/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `verifyCode` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `verifyCodeExpiry` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "isAcceptingMessages" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL,
+ADD COLUMN     "verifyCode" TEXT NOT NULL,
+ADD COLUMN     "verifyCodeExpiry" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
