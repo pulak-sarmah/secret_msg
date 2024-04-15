910e86c99a158b4fa85f56f104a1424da2561277
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-15 10:21:27 +0530

    Refactor prismaClient utility

--- apps/web/utils/prismaClient.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@secret-hub/db/client'
-
-const prisma = new PrismaClient({
-  log: ['query'],
-})
-export default prisma
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['warn', 'error'],
+  })
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
+export default prisma
 export * from '@prisma/client'
