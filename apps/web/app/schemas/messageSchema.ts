c63ebb9b156cad8316b9cb9bea023be36b783220
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-15 11:21:05 +0530

    Add zod schemas for message, sign in, sign up, and verification

---
+++ apps/web/app/schemas/messageSchema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const messageSchema = z.object({
+  content: z
+    .string()
+    .min(10, { message: 'Message must atleat 10 charecter' })
+    .max(300, { message: 'Message must be at most 300 charecter' }),
+})
