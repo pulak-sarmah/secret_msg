162216ceee675a1d3d8cfbc0d2ba250b7e1658e4
pulak-sarmah <pulaksarmah333@gmail.com>
2024-04-17 09:50:39 +0530

    Add API route for checking username availability

---
+++ apps/web/app/api/check-username/route.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod'
+import prisma from '@secret-hub/db/client'
+import { usernameValidation } from '../../schemas/signUpSchema'
+import { NextRequest } from 'next/server'
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+})
+
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = new URL(request.url)
+    const queryParam = {
+      username: searchParams.get('username'),
+    }
+    const result = UsernameQuerySchema.safeParse(queryParam)
+    if (!result.success) {
+      return Response.json(
+        {
+          success: false,
+          message: result.error.format().username?._errors || [],
+        },
+        { status: 400 }
+      )
+    }
+    const { username } = result.data
+
+    const existingVerifiedUser = await prisma.user.findUnique({
+      where: { username, isVerified: true },
+    })
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        { success: false, message: 'Username already exists' },
+        { status: 400 }
+      )
+    }
+
+    return Response.json({ success: true, message: 'Username is available' })
+  } catch (error) {
+    return Response.json(
+      { success: false, error: 'Error checking username' },
+      { status: 500 }
+    )
+  }
+}
